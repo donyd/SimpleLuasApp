5cbe6b7f65eeaabdfba171f61d1bb186c4a9a34a
= <bonebare@gmail.com>
2017-08-27 16:43:27 +0100

    added CSS and AJAX call

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var getLuasTime = function(){
+  var httpRequest;
+
+  makeRequest();
+
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+
+    if(!httpRequest){
+      alert('Unable to create a XMLHTTP instance');
+      return false;
+    }
+
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', 'http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop=sti&encrypt=false');
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE){
+      if(httpRequest.status === 200){
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request');
+      }
+    }
+  }
+
+};
