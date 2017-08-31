462d7a05bdf9d93e7863176d3e7b747bd0b1aa03
= <bonebare@gmail.com>
2017-08-31 18:28:56 +0100

    pushing raw response msg take two

--- js/script.js
+++ js/script.js
@@ -21,7 +21,8 @@ var getLuasTime = function(){
       if(httpRequest.status === 200){
         alert(httpRequest.responseText);
       } else {
-        alert('There was a problem with the request\n' + httpRequest.responseText);
+        alert('There was a problem with the request\n');
+		alert(httpRequest.responseText);
       }
     }
   }
